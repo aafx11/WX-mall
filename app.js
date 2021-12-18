ae0d5743ab098de85e7fc884d715657b4127b04d
fx <903921845@qq.com>
2021-12-18 13:18:20 +0800

    first

---
+++ app.js
@@ -0,0 +1,32 @@
+// app.js
+App({
+  onLaunch() {
+    wx.cloud.init({
+      env: 'cloud1-7gunq0vc9a762fcd',
+      traceUser: true
+    })
+
+    this.getCartCount()
+  },
+
+  globalData: {
+    userInfo: null,
+    number: ''
+  },
+
+  // 用户登录后，获取购物车商品数量
+  getCartCount() {
+    if (wx.getStorageSync('storage_info') == 1) {
+      console.log('用户登录后')
+      let cart = wx.getStorageSync('cart') || []
+      if (cart.length > 0) {
+        this.globalData.number = cart.length
+      } else {
+        this.globalData.number = ''
+
+      }
+    } else{
+      this.globalData.number = ''
+    }
+  }
+})
