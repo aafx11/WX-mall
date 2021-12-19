04d9d22c3194a5c6e24093cf204c7cffc4140c09
fx <903921845@qq.com>
2021-12-19 12:10:03 +0800

    has env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 App({
   onLaunch() {
     wx.cloud.init({
-      env: '',
+      env: 'cloud1-7gunq0vc9a762fcd',
       traceUser: true
     })
 
@@ -14,5 +14,19 @@ App({
     number: ''
   },
 
+  // 用户登录后，获取购物车商品数量
+  getCartCount() {
+    if (wx.getStorageSync('storage_info') == 1) {
+      console.log('用户登录后')
+      let cart = wx.getStorageSync('cart') || []
+      if (cart.length > 0) {
+        this.globalData.number = cart.length
+      } else {
+        this.globalData.number = ''
 
+      }
+    } else{
+      this.globalData.number = ''
+    }
+  }
 })
