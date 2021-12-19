3ed84083014b46a06210cdc647f7e15f5e84dc98
aafx11 <84850314+aafx11@users.noreply.github.com>
2021-12-19 12:06:58 +0800

    Update app.js
    update app.js

--- app.js
+++ app.js
@@ -14,19 +14,5 @@ App({
     number: ''
   },
 
-  // 用户登录后，获取购物车商品数量
-  getCartCount() {
-    if (wx.getStorageSync('storage_info') == 1) {
-      console.log('用户登录后')
-      let cart = wx.getStorageSync('cart') || []
-      if (cart.length > 0) {
-        this.globalData.number = cart.length
-      } else {
-        this.globalData.number = ''
 
-      }
-    } else{
-      this.globalData.number = ''
-    }
-  }
 })
